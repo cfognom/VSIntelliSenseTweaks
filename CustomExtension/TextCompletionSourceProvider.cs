9138bdbfb5de6e10d546b1c4d7e9c554382a82d7
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2023-02-24 13:39:08 +0100

    Add project files.

---
+++ CustomExtension/TextCompletionSourceProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CustomExtension
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("plaintext")]
+    [Name("token completion")]
+    internal class TestCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new TestCompletionSource(this, textBuffer);
+        }
+    }
+}
