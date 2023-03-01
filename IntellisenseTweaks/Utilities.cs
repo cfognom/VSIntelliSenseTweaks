7d8b63cd71614fbd971a54da8472b4f7ce2c50d6
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2023-03-01 12:35:01 +0100

    Add utilities class

---
+++ IntellisenseTweaks/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace IntellisenseTweaks
+{
+    internal static class Utilities
+    {
+        public static TextSpan GetTextSpan(ref this SnapshotSpan span)
+        {
+            TextSpan textSpan = new TextSpan();
+
+            var startPoint = span.Start;
+            textSpan.iStartIndex = startPoint.Position;
+            textSpan.iStartLine = startPoint.GetContainingLineNumber();
+
+            var endPoint = span.End;
+            textSpan.iEndIndex = endPoint.Position;
+            textSpan.iEndLine = endPoint.GetContainingLineNumber();
+
+            return textSpan;
+        }
+    }
+}
