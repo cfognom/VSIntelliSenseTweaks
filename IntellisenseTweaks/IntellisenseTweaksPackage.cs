bddf07682efe1bc7adfd4eaaf719731ef54007e5
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2023-03-01 19:35:14 +0100

    Clean up package init

--- IntellisenseTweaks/IntellisenseTweaksPackage.cs
+++ IntellisenseTweaks/IntellisenseTweaksPackage.cs
@@ -2,10 +2,7 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Microsoft.VisualStudio.LanguageServer;
-using Microsoft.VisualStudio.Package;
 using Task = System.Threading.Tasks.Task;
-using System.ComponentModel.Design;
 
 namespace IntellisenseTweaks
 {
@@ -37,8 +34,6 @@ namespace IntellisenseTweaks
 
         #region Package Members
 
-        public static LanguageService languageService;
-
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
@@ -48,9 +43,6 @@ namespace IntellisenseTweaks
         /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            IServiceContainer serviceContainer = this as IServiceContainer;
-            languageService = (LanguageService)serviceContainer.GetService(typeof(LanguageService));
-            if (languageService == null) { throw new Exception(); }
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
