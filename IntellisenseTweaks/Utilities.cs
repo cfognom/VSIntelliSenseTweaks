eca28a566b1b7663729e74cc494ace6eba87a113
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2023-03-07 18:22:08 +0100

    Add BitSpan

--- IntellisenseTweaks/Utilities.cs
+++ IntellisenseTweaks/Utilities.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TextManager.Interop;
+using System.Collections;
+using Microsoft;
+using System.Diagnostics;
 
 namespace IntellisenseTweaks
 {
@@ -30,6 +33,45 @@ namespace IntellisenseTweaks
         public static ReadOnlySpan<char> Slice(this ReadOnlySpan<char> word, Span span)
         {
             return word.Slice(span.Start, span.Length);
+        }
+
+        public ref struct BitSpan
+        {
+            private Span<int> data;
+
+            public static int GetRequiredIntCount(int n_bits)
+            {
+                Debug.Assert(n_bits > 0);
+                return (n_bits - 1) / 32 + 1;
+            }
+
+            public BitSpan(Span<int> data)
+            {
+                this.data = data;
+            }
+
+            public bool this[int index]
+            {
+                get
+                {
+                    var intIndex = Math.DivRem(index, 32, out var bitIndex);
+                    var mask = 1 << bitIndex;
+                    return (data[intIndex] & mask) == mask;
+                }
+                set
+                {
+                    var intIndex = Math.DivRem(index, 32, out var bitIndex);
+                    var mask = 1 << bitIndex;
+                    if (value)
+                    {
+                        data[intIndex] |= mask;
+                    }
+                    else
+                    {
+                        data[intIndex] &= ~mask;
+                    }
+                }
+            }
         }
     }
 }
