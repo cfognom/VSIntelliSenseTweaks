adfb5c112bce48c72e02268e7c063056c066915b
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2023-03-09 17:03:57 +0100

    Add bitfield operators

--- VSIntelliSenseTweaks/Utilities/BitField64.cs
+++ VSIntelliSenseTweaks/Utilities/BitField64.cs
@@ -27,5 +27,12 @@ namespace VSIntelliSenseTweaks.Utilities
             var mask = 1ul << index;
             data &= ~mask;
         }
+
+        public static bool operator ==(BitField64 x, BitField64 y) => x.data == y.data;
+        public static bool operator !=(BitField64 x, BitField64 y) => x.data != y.data;
+        public static BitField64 operator &(BitField64 x, BitField64 y) => new BitField64 { data = x.data & y.data };
+        public static BitField64 operator |(BitField64 x, BitField64 y) => new BitField64 { data = x.data | y.data };
+        public static BitField64 operator ^(BitField64 x, BitField64 y) => new BitField64 { data = x.data ^ y.data };
+        public static BitField64 operator ~(BitField64 x) => new BitField64 { data = ~x.data };
     }
 }
