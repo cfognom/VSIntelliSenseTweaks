85f10bc95f8b0f5c17f0d70fe56dc3f8d99d5b8a
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2023-03-16 13:24:21 +0100

    Add ToString

--- VSIntelliSenseTweaks/Utilities/BitField64.cs
+++ VSIntelliSenseTweaks/Utilities/BitField64.cs
@@ -1,4 +1,7 @@
-﻿namespace VSIntelliSenseTweaks.Utilities
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+
+namespace VSIntelliSenseTweaks.Utilities
 {
     internal struct BitField64
     {
@@ -39,5 +42,10 @@
         public static BitField64 operator |(BitField64 x, BitField64 y) => new BitField64 { data = x.data | y.data };
         public static BitField64 operator ^(BitField64 x, BitField64 y) => new BitField64 { data = x.data ^ y.data };
         public static BitField64 operator ~(BitField64 x) => new BitField64 { data = ~x.data };
+
+        public override string ToString()
+        {
+            return Convert.ToString((long)data, 2);
+        }
     }
 }
